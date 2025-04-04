e637cc9899c94e092b64aeac109e4d8d0a719807
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2025-04-04 16:25:53 -0400

    bolded font in skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -103,7 +103,7 @@ const Skills = ({ isLightMode }) => {
                                     <span className="absolute bottom-[-2rem] left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-800 text-white text-xs font-mono rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none max-w-[150px] whitespace-normal z-50">
                                         {skill.description}
                                     </span>
-                                    <h4 className={`mt-2 font-mono text-sm ${isLightMode ? 'text-black' : 'text-black'}`}>
+                                    <h4 className={`mt-2 font-mono text-sm ${isLightMode ? 'text-black ' : 'text-black font-bold'}`}>
                                         {skill.label}
                                     </h4>
                                 </div>
